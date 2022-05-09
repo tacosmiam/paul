d0ac615a49a6e91e7da8d11adfd9829dda736a05
tacosmiam <103418985+tacosmiam@users.noreply.github.com>
2022-05-09 11:14:24 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,66 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(200)
+})
